ba6bd90e0f0f208f984452bb91ad5977ec7b6124
Ledger User <ledger@example.com>
2026-01-04 14:56:36 +0530

    Migration: Change attendance status from String to Float for OT multipliers

---
+++ prisma/migrations/20260104_change_attendance_status_to_float/migration.sql
@@ -0,0 +1,8 @@
+/*
+  Warnings:
+
+  - Changed the type of `status` on the `attendances` table. No cast could be determined, the database may contain data that cannot be cast by the new integer type.
+
+*/
+-- AlterTable
+ALTER TABLE "attendances" ALTER COLUMN "status" SET DATA TYPE DOUBLE PRECISION;
